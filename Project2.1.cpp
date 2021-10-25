92511443178583e25984be0deef58e1fd25ee87c
Ira <Ira@DESKTOP-FFERK89>
2021-10-25 22:17:24 +0300

    first commit

--- Project2.1.cpp
+++ Project2.1.cpp
@@ -14,7 +14,7 @@ int main()
 	double z2; // результат обчислення 2-го виразу
 	cout << "b = "; cin >> b;
 	z1 = (1.0 * (sqrt(2 * b + (2 * sqrt(b * b - 4))))) / (sqrt(b * b - 4) + b + 2);
-	z2 = 1 / sqrt(b + 2);
+	z2 = 1 / sqrt(b + 2);//change one
 	cout << endl;
 	cout << "z1 = " << z1 << endl;
 	cout << "z2 = " << z2 << endl;
